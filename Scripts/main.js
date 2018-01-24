1dcddec96e09989eef2a0961861f0d26b4fe402a
Bùi Tuấn Anh <herosf2006@yahoo.com>
2018-01-24 17:32:01 +0700

    Change content item

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,4 @@
+// newElement()
 filterSelection("all")
 
 function filterSelection(c) {
@@ -15,7 +16,9 @@ function AddClass(element, name) {
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
   }
 }
 
@@ -25,7 +28,7 @@ function RemoveClass(element, name) {
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
   element.className = arr1.join(" ");
@@ -35,7 +38,7 @@ function RemoveClass(element, name) {
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
@@ -43,12 +46,34 @@ for (var i = 0; i < btns.length; i++) {
 }
 
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-   document.getElementById("demo").innerHTML = "Welcome!";
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        document.getElementById("demo").innerHTML = myObj.name;
-    }
+xmlhttp.onreadystatechange = function () {
+  document.getElementById("demo").innerHTML = "Welcome!";
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObj.name;
+  }
 };
-xmlhttp.open("GET", "https://raw.githubusercontent.com/herosf2006/SALEWEB/master/Scripts/data.json", true);
+xmlhttp.open("GET", "https://raw.githubusercontent.com/herosf2006/SALEWEB/master/Resource/JSON/data.json", true);
 xmlhttp.send();
+
+// function newElement(category, name, url, value, quantity) {
+function newElement() {
+  var category = document.createElement("div");
+  // var t = document.createTextNode(name);
+  category.className = ("Category 1");
+  document.getElementById("grid").appendChild(category);
+  var content = document.createElement("div");
+  category.appendChild(content);
+  content.className = ("content");
+  var img = document.createElement("img");
+  content.appendChild(img);
+  img.src = "https://www.w3schools.com/w3images/mountains.jpg";
+  img.style = "width:100%";
+
+  var h4 = document.createElement("h4");
+  var p = document.createElement("p");
+  h4.innerHTML = "Type 1";
+  p.innerHTML = "Blah blah blah";
+  content.appendChild(h4);
+  content.appendChild(p);
+}
